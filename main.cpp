845a8303f42e152386d803bbef73db4aedb01b12
George <georgesucuzhanay100@gmail.com>
2022-02-17 11:56:50 -0500

    completed task c compare basin in elevation west and east

--- main.cpp
+++ main.cpp
@@ -7,7 +7,10 @@ int main()
   
   std::cout << "East Storage for 01/09/2018:  " << get_east_storage("01/09/2018") << std::endl;
   std::cout << "Minimum storage in East basin: "<< get_min_east() << std::endl;
-  std::cout << "Maximum storage in East basin: " << get_max_east() <<std::endl;
+  std::cout << "Maximum storage in East basin: " << get_max_east() << std::endl;
+  std::cout << "Compare func: " << compare_basins("02/18/2018") << std::endl;
+  std::cout << "Compare func: " << compare_basins("09/13/2018") << std::endl;
+
 
   return 0;
 }
--- reservoir.cpp
+++ reservoir.cpp
@@ -1,4 +1,3 @@
-// add your code
 #include <iostream>
 #include <cstdlib>
 #include "reservoir.h"
@@ -114,3 +113,40 @@ double get_max_east()
     return max_value;
 };
 
+std::string compare_basins(std::string date)
+{
+    std::ifstream fin("Current_Reservoir_Levels.tsv");
+    if (fin.fail()) 
+    {
+        std::cerr << "File cannot be opened for reading." << std::endl;
+        exit(1); // exit if failed to open the file
+
+    }
+    else
+    {
+        std::string junk, date;        // new string variable
+        std::getline(fin, junk);
+        double eastEl, westSt, westEl, eastSt;
+
+        while(fin >> date >> eastSt >> eastEl >> westSt >> westEl)
+        { 
+            // this loop reads the file line-by-line
+            // extracting 5 values on each iteration 
+            fin.ignore(INT_MAX, '\n'); //skips to the end of line, 
+                                //ignorring the remaining columns 
+        
+            // for example, to print the date and East basin storage:
+            if(westEl > eastEl){
+                return "West";
+            }
+            else if(westEl < eastEl){
+                return "East";
+            }
+            else{
+                return "Equal";
+            }
+        };
+        fin.close();
+    };
+    return "";
+}
