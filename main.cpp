43d55af754400c650981f44da556367ffab4fa96
George <georgesucuzhanay100@gmail.com>
2022-02-17 00:11:06 -0500

    returned east storage in task a

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 // add your code as appropriate 
 #include <iostream>
 #include "reservoir.h"
+
+double get_east_storage(std::string date);
+
+
 int main()
+
 {
   
 
-  
+  std::cout << "Return 01/09/2018:  " << get_east_storage("01/09/2018") << std::endl;
+
   return 0;
 }
--- reservoir.cpp
+++ reservoir.cpp
@@ -3,14 +3,40 @@
 #include <cstdlib>
 #include "reservoir.h"
 #include <fstream>
+#include <climits>
 
-double get_east_storage(std::string date);
+double get_east_storage(std::string date)
+{
+    std::ifstream fin("Current_Reservoir_Levels.tsv");
+    double eastSt = -1.0;
+    if (fin.fail()) 
+    {
+        std::cerr << "File cannot be opened for reading." << std::endl;
+        exit(1); // exit if failed to open the file
 
-std::ifstream fin("Current_Reservoir_Levels.tsv");
+    }
+    else
+    {
+        std::string junk, date;        // new string variable
+        std::getline(fin, junk);
+        double eastEl, westSt, westEl;
 
+        while(fin >> date >> eastSt >> eastEl >> westSt >> westEl)
+        { 
+            // this loop reads the file line-by-line
+            // extracting 5 values on each iteration 
+            fin.ignore(INT_MAX, '\n'); //skips to the end of line, 
+                                //ignorring the remaining columns 
+        
+            // for example, to print the date and East basin storage:
+            if(date == date){
+                fin.close();
+				return eastSt;
+            }
+            // std::cout << date << " " << eastSt << std::endl;
+        };
+        fin.close();
+    };
+    return eastSt;
+};
 
-
-if (fin.fail()) {
-    cerr << "File cannot be opened for reading." << endl;
-    exit(1); // exit if failed to open the file
-}
